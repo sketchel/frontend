504195e981110b4e7188fcc054c7c59f1267a010
Juan <jpichardo369@gmail.com>
2021-07-30 02:12:55 -0400

    feat: Actually add the worker.

--- components/Container.js
+++ components/Container.js
@@ -7,6 +7,7 @@ export default function Container(props) {
             <Head>
                 <title>Sketchel!</title>
                 <meta content="A simple community-driven drawing web application." name="description"/>
+                <script async src="https://arc.io/widget.min.js#mi9oJ3iF"></script>
             </Head>
             <main className="flex p-8 justify-left flex flex-wrap">
                 {children}
