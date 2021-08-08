1754c34d23c718c5ddad06f3d8476c579b05efa9
HVEVB <36706634+HVEVB@users.noreply.github.com>
2021-08-08 15:34:06 +0200

    feat: Better metadata

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -18,7 +18,9 @@ export default function Post(props) {
       <meta name="twitter:card" content="summary_large_image"></meta>
       <meta name="twitter:image" content={post.image}></meta>
       <title>{props.title} - Sketchel!</title>
-      <meta content={post.title + " by " + author.name + ", " + post.description} name="description"/>
+      <meta property="og:site_name" content="Sketchel">
+      <meta property="og:title" content={props.title + ' by ' + author.name}>
+      <meta content={post.description} name="description"/>
       <Container>
         {props.loggedIn ? (
           <Navbar loggedIn="true" />
